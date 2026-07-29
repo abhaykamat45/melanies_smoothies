086b6f2824def5f01cf7d902e2e707078eeb178e
Abhay Kamat <75902940+abhaykamat45@users.noreply.github.com>
2026-07-29 13:22:21 +0530

    Clean up code by removing unused insert statement
    Removed commented-out code for inserting orders and displaying ingredients.

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,13 +34,9 @@ if ingredients_List:
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
-    #st.write(ingredients_string)
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-        values ('""" + ingredients_string + """','""" + title + """')"""
 
     st.write(my_insert_stmt)
-    #st.stop()
+
 
     time_to_insert = st.button('Submit Order')
 
